9421175b77da754e4b0435331b135f1ee2eb037d
Cameron Pavey <cpave3@gmail.com>
2020-05-15 20:47:25 +1000

    add selection sort

---
+++ selection-sort/selectionSort.js
@@ -0,0 +1,22 @@
+const selectionSort = (items) => {
+  let smallestIndex;
+  for (let divider = 0; divider < items.length - 1; divider++) {
+    smallestIndex = divider;
+
+    for (let i = divider; i < items.length; i++) {
+      if (items[i] < items[smallestIndex]) {
+        smallestIndex = i;
+      }
+    }
+
+    if (smallestIndex !== divider) {
+      const temp = items[divider];
+      items[divider] = items[smallestIndex];
+      items[smallestIndex] = temp;
+    }
+  }
+
+  return items;
+};
+
+module.exports = selectionSort;
---
+++ selection-sort/selectionSort.test.js
@@ -0,0 +1,17 @@
+const selectionSort = require('./selectionSort');
+
+test('sorts array', () => {
+    const input = [4,3,2,5,8];
+    const expected = [2,3,4,5,8];
+    const actual = selectionSort(input);
+
+    expect(actual).toStrictEqual(expected);
+});
+
+test('sorts array 2', () => {
+    const input = [100,10,15,23,2,9,28,1,36,1];
+    const expected = [1,1,2,9,10,15,23,28,36,100];
+    const actual = selectionSort(input);
+
+    expect(actual).toStrictEqual(expected);
+});
